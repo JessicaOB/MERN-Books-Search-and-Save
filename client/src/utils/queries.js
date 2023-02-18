bb1d88cc8ead5697766aa39ec0e3add80d546f91
Jessica Oboh <jessicarichardson@Jessicas-MacBook-Pro.local>
2023-02-18 16:24:34 -0700

    finished back end updates, started front end

---
+++ client/src/utils/queries.js
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  query user {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+
+
